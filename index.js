8c06108b03535d4ede9ea7227133e33501f2bb6b
reyeon1209 <reyeon5368@naver.com>
2020-08-20 18:42:14 +0900

    보안

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+const config = require('./config/key')
 const { User } = require("./models/User")
 
 // application/x-www-form-urlencoded 를 분석해서 가져옴
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:test1234@boilerplate.tcbwq.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true, 
