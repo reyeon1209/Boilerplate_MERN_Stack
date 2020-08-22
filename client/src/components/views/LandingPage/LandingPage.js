b02b11b229ef9cfd2b7ebb6a566e288d13f23235
reyeon1209 <reyeon5368@naver.com>
2020-08-22 21:58:35 +0900

    Redux를 사용한 로그아웃페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,30 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 
-function LandingPage() {
+function LandingPage(props) {
     useEffect(() => {
         axios.get('/api/hello').then(response=> { console.log(response) })
     }, [])
+
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                if (response.data.success) {
+                    props.history.push('/login')
+                } else {
+                    alert('Failed To Logout')
+                }
+            })
+    }
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height: '100vh' }}>
             <h2>시작 페이지</h2>
+
+            <button onClick={onClickHandler}>
+                로그아웃
+            </button>
         </div>
     )
 }
