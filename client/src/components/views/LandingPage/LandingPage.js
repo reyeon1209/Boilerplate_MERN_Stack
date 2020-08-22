5f0228017674f5ccd58bbe5c80e24ce6cec95cc0
reyeon1209 <reyeon5368@naver.com>
2020-08-22 18:27:58 +0900

    Data Flow & Axios

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { response } from 'express'
+import axios from 'axios'
 
 function LandingPage() {
+    userEffect(() => {
+        axios.get('/api/hello').then(response=> { console.log(response) })
+    }, [])
+
     return (
-        <div>
-            LandingPage
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh' }}>
+            <h2>시작 페이지</h2>
         </div>
     )
 }
